4f071e0f9f432b54bf5a39d3c6254ee013904e34
bamjam101 <herejamil123@gmail.com>
2023-03-01 11:37:23 +0530

    Implemented a protected route to only let logged in user access the dashboard.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
-import { Routes } from "react-router-dom";
+import { Routes, useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -11,6 +11,14 @@ import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+
+const ProtectedRoute = ({ children }) => {
+  const user = useSelector((state) => state.global.userId);
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -23,7 +31,14 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" />} />
-              <Route path="/dashboard" element={<Dashboard />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
             </Route>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,12 +2,6 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 const Dashboard = () => {
-  const userId = useSelector((state) => state.global.userId);
-  useEffect(() => {
-    if (!userId) {
-      navigate("/");
-    }
-  });
   return <div>Dashboard</div>;
 };
 
